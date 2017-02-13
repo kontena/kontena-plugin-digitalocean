e913daf2d341e4b0f780e26cd70f513462f5d7b1
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-02-13 11:00:06 -0500

    initial master terminate command

--- lib/kontena/plugin/digital_ocean/master_command.rb
+++ lib/kontena/plugin/digital_ocean/master_command.rb
@@ -1,8 +1,10 @@
 require_relative 'master/create_command'
+require_relative 'master/terminate_command'
 
 class Kontena::Plugin::DigitalOcean::MasterCommand < Kontena::Command
 
   subcommand "create", "Create a new master to DigitalOcean", Kontena::Plugin::DigitalOcean::Master::CreateCommand
+  subcommand "terminate", "Terminate DigitalOcean master", Kontena::Plugin::DigitalOcean::Master::TerminateCommand
 
   def execute
   end
