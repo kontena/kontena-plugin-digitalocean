47411f524d1509d912d949aa0cde23fc0e81afe8
Jari Kolehmainen <jari.kolehmainen@gmail.com>
2017-07-20 07:51:26 +0300

    show droplet type in prompt

--- lib/kontena/plugin/digital_ocean/prompts.rb
+++ lib/kontena/plugin/digital_ocean/prompts.rb
@@ -1,7 +1,7 @@
 module Kontena::Plugin::DigitalOcean::Prompts
   def ask_do_token
     if self.token.nil?
-      prompt.ask('DigitalOcean API token: ', echo: false)
+      prompt.ask('DigitalOcean API token:', echo: false)
     else
       self.token
     end
@@ -9,7 +9,7 @@ module Kontena::Plugin::DigitalOcean::Prompts
 
   def ask_droplet_region(do_token)
     if self.region.nil?
-      prompt.select("Choose a datacenter region: ") do |menu|
+      prompt.select("Choose a datacenter region:") do |menu|
         do_client = DropletKit::Client.new(access_token: do_token)
         do_client.regions.all.sort_by{|r| r.slug }.each{ |region|
           menu.choice region.name, region.slug
@@ -22,12 +22,13 @@ module Kontena::Plugin::DigitalOcean::Prompts
 
   def ask_droplet_size(do_token, do_region)
     if self.size.nil?
-      prompt.select("Choose droplet size: ") do |menu|
+      prompt.select("Choose droplet size:") do |menu|
         do_client = DropletKit::Client.new(access_token: do_token)
         do_client.sizes.all.to_a.select{ |s| s.memory > 1000 }.sort_by{|s| s.memory }.each{ |size|
+          #p size
           if size.regions.include?(do_region)
             memory = size.memory.to_i / 1024
-            menu.choice "#{memory}GB/#{size.vcpus}CPU/#{size.disk}GB ($#{size.price_monthly.to_i}/mo)", size.slug
+            menu.choice "#{size.slug}: #{memory}GB/#{size.vcpus}CPU/#{size.disk}GB ($#{size.price_monthly.to_i}/mo)", size.slug
           end
         }
       end
@@ -43,7 +44,7 @@ module Kontena::Plugin::DigitalOcean::Prompts
         n['labels'] && n['labels'].include?('provider=digitalocean'.freeze)
       }
       raise "Did not find any nodes with label provider=digitalocean" if nodes.size == 0
-      prompt.select("Select node: ") do |menu|
+      prompt.select("Select node:") do |menu|
         nodes.sort_by{|n| n['node_number'] }.reverse.each do |node|
           initial = node['initial_member'] ? '(initial) ' : ''
           menu.choice "#{node['name']} #{initial}", node['name']
@@ -55,7 +56,7 @@ module Kontena::Plugin::DigitalOcean::Prompts
   end
 
   def ask_channel
-    prompt.select('Select CoreOS channel') do |menu|
+    prompt.select('Select Container Linux channel:') do |menu|
       menu.choice 'Stable', 'stable'
       menu.choice 'Beta', 'beta'
       menu.choice 'Alpha', 'alpha'
